e96bba16d5028a34f273f8b78d7083959cb3365e
tomfinnegan <thomasjosephfinnegan@gmail.com>
2021-05-08 19:22:22 +0000

    added content to style.css

--- assets/index.js
+++ assets/index.js
@@ -2,7 +2,7 @@
 
 var modal =document.getElementById("myModal");
 
-//get the buton tah opens the modal 
+//get the buton that opens the modal 
 
 var btn = document.getElementById("myBtn");
 
@@ -16,4 +16,19 @@ btn.onclick = function () {
     modal.style.display = "block";
 }
 
+// when the user clicks on <span> (x), close the modal
+
+span.onclick = function (){
+    modal.style.display = "none";
+}
+
+// when the user clicks anywhere outside the modal ,close it
+
+
+window.onclick = function(event) {
+    if(event.target == modal) {
+
+       modal.style.display = "none";
+    }
+}
 
