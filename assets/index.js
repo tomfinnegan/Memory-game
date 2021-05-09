14961a12835a6bea5a4cd1d60125f8df5c860a9e
tomfinnegan <thomasjosephfinnegan@gmail.com>
2021-05-09 10:21:43 +0000

    added to index.js with modal information

--- assets/index.js
+++ assets/index.js
@@ -1,34 +1,38 @@
+// Reference w3Schools for the code for modal
+///////////////////////////////////////////////
+
+
 // Get the modal
 
-var modal =document.getElementById("myModal");
+var modal = document.getElementById("myModal");
 
-//get the buton that opens the modal 
+
+// Get the button that opens the modal
 
 var btn = document.getElementById("myBtn");
 
-//Get the <span> elements that closes the modal
+// Get the <span> element that closes the modal
 
 var span = document.getElementsByClassName("close")[0];
 
-// when the user clicks the button , open modal
+// When the user clicks the button, open the modal
 
 btn.onclick = function () {
-    modal.style.display = "block";
+    modal.style.display ="block";
 }
 
-// when the user clicks on <span> (x), close the modal
+// when the user clicks on <span> (x) , close the modal
 
-span.onclick = function (){
-    modal.style.display = "none";
+span.onclick = function () {
+    modal.style.display ="none"; 
 }
 
-// when the user clicks anywhere outside the modal ,close it
+// when the user clicks anywhere outside the modal, close it
 
-
-window.onclick = function(event) {
-    if(event.target == modal) {
-
-       modal.style.display = "none";
+window.onclick = function (event) {
+    if (event.target == modal) {
+        this.modal.style.display ="none";
     }
 }
+
 
