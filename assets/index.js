744a306e2e75e69bba09999d3fb46105db76d43d
tomfinnegan <thomasjosephfinnegan@gmail.com>
2021-05-13 20:16:26 +0000

    index.js

--- assets/index.js
+++ assets/index.js
@@ -1,32 +1,5 @@
 
 
-// Get the modal
-var modal = document.getElementById("myModal");
-
-// Get the button that opens the modal
-var btn = document.getElementById("myBtn");
-
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
-
-// When the user clicks on the button, open the modal
-btn.onclick = function() {
-  modal.style.display = "block";
-}
-
-// When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-  modal.style.display = "none";
-}
-
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
-  }
-}
-
-
 const cards = document.querySelectorAll('.memory-card');
 
 let hasFlippedCard;
@@ -36,162 +9,193 @@ let moves;
 let remainingCards = 12;
 let counter = document.querySelector(".moves");
 
-const stars = document.querySelectorAll(".fa-star");
+const stars = document.querySelectorAll(".fa-stars");
 
 let starsList = document.querySelectorAll(".stars li");
 
-/** Starts a new game */
+/** start new game */
 
 document.body.onload = resetGame();
 
+/** card flipping */
 
 function flipCard() {
 
     if (lockBoard) return;
-    if (this === firstCard) return;
+    if ( this === firstCard) return;
 
-    this.classlist.add('flip');
+    this.classList.add('flip');
 
     if (!hasFlippedCard) {
         hasFlippedCard = true;
         firstCard = this;
         moveCounter();
-
     } else {
-
         secondCard = this;
-        checkForMatch();
+        checkforMatch();
     }
 }
 
-/** Move counter  timer */
+/**move counter /timer/stars */
 
- function moveCounter() {
-     moves++;
-     counter.innerHTML = moves;
+function moveCounter() {
+    moves++;
+    counter.innerHTML = moves;
 
-     if (moves ==1) {
-         second = 0;
-         minute = 0;
-         hour = 0 ;
-         startTimer();
-     } 
+    if (moves == 1) {
+        second = 0;
+        minute = 0;
+        hour = 0;
+        startTimer();
+    }
 
     if (moves > 8 && moves < 12) {
         for (i = 0; i < 3; i++) {
-         if (i > i) {
-             stars[i].style.visibility = "collapse";
-         }
-        }
-    }
-
-    else if (moves > 13) {
-        for (i = 0; < 3; i++) {
-            if (i > 0) {
-               stars[i].style.visibility = "collapse"; 
+            if (i > 1) {
+                stars[i].style.visibility = "collapse";
             }
         }
     }
- } 
+    else if (moves > 13) {
+        for (i = 0; i < 3; i++) {
+            if (i > 0) {
+                stars[i].style.visibility = "collapse";
+            }
+        }
+    }
+}
 
- /** timer and clock */
+/** timer clock */
 
- var second = 0, minute = 0; hour = 0;
- var timer = document.querySelector(".timer");
- var interval;
- function startTimer() {
-     interval = setInterval(function () {
-         if (remainingCards ==0) {
-             return;
-         }
-         timer.innerHTML = minute + "mins " + second + "secs";
-         second++;
-         if (second == 60) {
-             minute++;
-             second = 0;
-         }
-         if (minute == 60) {
-             hour++;
-             minute = 0;
-         }
-     }, 1000) ; 
- }
+var second = 0, minute = 0; hour =0;
+var timer = document.querySelector(".timer");
+var interval;
+function startTimer() {
+    interval = setInterval(function () {
+       if (remainingCards == 0) {
+           return;
+       }
+       timer.innerHTML = minute + "mins" + second + "secs";
+       second++;
+       if (second == 60) {
+           minute++;
+           second = 0;
+       } 
+           if (minute == 60) {
+               hour++;
+               minute = 0;
+           }
+        }, 1000);
+   }
+/**check for matching pair */
 
- /** two cards flipping over **/
 
- function checkForMatch() {
-
+function checkforMatch() {
     if (firstCard.dataset.framework === secondCard.dataset.framework) {
 
         disableCards();
     } else {
-        unflipCards(); 
+        unflipCards();
     }
- }
+}
 
- /** unclickable matched cards */
+/**unclickable */
 
- function disableCards() {
-     remainingCards -= 2;
-      
-     firstCard.removeEventListener('click' , flipCard);
-     secondCard.removeEventListener('click',flipcard);
-     
-     resetBoard();
-     if (remainingCards == 0) {
-         Well Done();
+function disableCards() {
+    remainingCards -=2;
 
-     }
- }
+    firstCard.remove.EventListener('click', flipCard);
+    secondCard.remove.EventListener('click', flipCard);
 
- /**cards that do not match */
+    resetBoard();
+    if (remainingCards == 0) {
+        congratulations();
+    }
+}
 
- function unflipCards() {
+/**cards dont match */
+
+function unflipCards() {
 
     lockBoard = true;
 
-    setTimeout(() => {
-        firstCard.classlist.remove('flip');
-        secondCard.classlist.remove('flip');
+    setTimeout (() => {
+        firstCard.classList.remove('flip');
+        secondCard.classList.remove('flip');
 
         resetBoard();
-
     }, 1500);
- }
+}
 
- function resetBoard() {
-     [hasFlippedCard, lockBoard] = [false, false];
-     [firstCard, secondCard] = [null, null];
- }
-
- /** cards being shuffled int new position */
+function resetBoard() {
+    [hasFlippedCard, lockBoard] = [false, false];
+    [firstCard, secondCard] = [null, null];
 
 
- function shuffle() {
-     cards.forEach(card =>) {
-         let randomPos = Math.floor(Math.random()  * 12);
-         card.style.order = randomPos;
-     });
- } 
+}
+/**cards being shuffled into new position */
 
- /**Well done modal */
+function shuffle () {
+    cards.forEach(card => {
+        let randomPos = Math.floor(Math.random() * 12);
+        card.style.order = randomPos;
+    });
+}
 
- function Well done () {
-     
-    document.getElementById("popup1").classlist.add("show");
+/**well done modal */
 
+function congratulations() {
+  
+    document.getElementById("popup1").classList.add("show");
+    
     var starRating = document.querySelector(".stars").innerHTML;
 
     document.getElementById("finalMove").innerHTML = moves;
     document.getElementById("starRating").innerHTML = starRating;
-    document.getElementById(totalTime).innerHTML = minute + "m " + second + "s ";
+    document.getElementById("totalTime").innerHTML = minute + "m " + second + "s ";
+}
 
- }
+/**close icon */
 
+function closeModal() {
+    document.getElementById("popup1").classList.remove("show");
+}
 
- /**close the modal**/
- 
- function closeModal () {
-     document.getElementById("popup1").classlist.remove("show");
- } 
- 
+/** users to play again */
+
+function playAgain() {
+    document.getElementById("popup1").classList.remove("show");
+    resetGame();
+
+}
+
+function resetGame() {
+    hasFlippedCard = false;
+    lockBoard = false;
+    firstCard = null;
+    secondCard = null;
+    remainingCards = 12;
+
+    cards.forEach(card => {
+        card.classList.remove('flip');
+    })
+
+    shuffle();
+
+    moves = 0;
+    counter.innerHTML = moves;
+
+    for (var i = 0; i < stars.length; i++){
+        stars[i].style.color = "#";
+        stars[i].style.visibility = "visible"; 
+    }
+
+    second = 0;
+    minute = 0;
+    hour = 0;
+    var timer = document.querySelector(".timer");
+    timer.innerHTML = "0 mins 0 secs";
+    clearInterval(interval);
+    cards.forEach(card => card.addEventListener('click' ,flipCard));
+
+}
