97f49ee1038fb159153ac21dc25385107624c58f
tomfinnegan <thomasjosephfinnegan@gmail.com>
2021-05-15 19:57:08 +0000

    changed css

--- assets/index.js
+++ assets/index.js
@@ -1,5 +1,3 @@
-
-
 const cards = document.querySelectorAll('.memory-card');
 
 let hasFlippedCard;
@@ -197,34 +195,30 @@ function resetGame() {
     timer.innerHTML = "0 mins 0 secs";
     clearInterval(interval);
     cards.forEach(card => card.addEventListener('click' ,flipCard));
-
-
-    // Get the modal
-
-    var modal = document.getElementById("myModal");
-
-    // Get the button that opens the modal
-
-    var btn = document.getElementById("myBtn");
-
-    // Get the <span> element that closes the modal 
-    var span = document.getElementsByClassName("close")[0];
-
-    // When the user clicks on the button, open the modal
-    btn.onclick = function() {
-        modal.style.display = "block";
-    }  
-
-    // When the user clicks on <span> (x), close the modal
-     span.onclick = function() {
-         modal.style.display = "none";
-     }
- 
-     // When the user clicks anywhere outside of  the modal, close it
-     window.onclick = function(event) {
-         if (event.target == modal) {
-             modal.style.display = "none";
-         }
-     }
-
 }
+   // Get the modal
+var modal = document.getElementById("myModal");
+
+// Get the button that opens the modal
+var btn = document.getElementById("myBtn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on the button, open the modal
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
+
