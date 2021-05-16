1b62172597fb28c16140ae38d4ada889dad09607
tomfinnegan <thomasjosephfinnegan@gmail.com>
2021-05-16 19:05:51 +0000

    changes made to pages

--- assets/index.js
+++ assets/index.js
@@ -102,8 +102,8 @@ function checkforMatch() {
 function disableCards() {
     remainingCards -=2;
 
-    firstCard.remove.EventListener('click', flipCard);
-    secondCard.remove.EventListener('click', flipCard);
+    firstCard.removeEventListener('click', flipCard);
+    secondCard.removeEventListener('click', flipCard);
 
     resetBoard();
     if (remainingCards == 0) {
@@ -222,3 +222,28 @@ window.onclick = function(event) {
   }
 }
 
+// Get the modal
+var modal = document.getElementById("myModal");
+
+// Get the button that opens the modal
+var btn = document.getElementById("myBtn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on the button, open the modal
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
