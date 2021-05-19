0b1f423d757387d6ba29df9e6aa829f26086b0c7
tomfinnegan <thomasjosephfinnegan@gmail.com>
2021-05-19 13:00:31 +0000

    ammended js

--- assets/index.js
+++ assets/index.js
@@ -50,14 +50,16 @@ function moveCounter() {
     if (moves > 8 && moves < 12) {
         for (i = 0; i < 3; i++) {
             if (i > 1) {
-                stars[i].style.visibility = "collapse";
+                /*'''''''''''
+              
+                **/
             }
         }
     }
     else if (moves > 13) {
         for (i = 0; i < 3; i++) {
             if (i > 0) {
-                stars[i].style.visibility = "collapse";
+            
             }
         }
     }
