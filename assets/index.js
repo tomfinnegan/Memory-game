e99988cfe583215121c97fb68d602246a20c6925
tomfinnegan <thomasjosephfinnegan@gmail.com>
2021-05-20 19:51:02 +0000

    changed elements on page

--- assets/index.js
+++ assets/index.js
@@ -1,3 +1,5 @@
+
+
 const cards = document.querySelectorAll('.memory-card');
 
 let hasFlippedCard;
@@ -9,11 +11,11 @@ let counter = document.querySelector(".moves");
 
 
 
-/** start new game */
+/** start new game **/
 
 document.body.onload = resetGame();
 
-/** card flipping */
+/** card flipping **/
 
 function flipCard() {
 
@@ -32,37 +34,22 @@ function flipCard() {
     }
 }
 
-/**move counter /timer/stars */
+/**move counter /timer/stars **/
 
 function moveCounter() {
     moves++;
     counter.innerHTML = moves;
-
     if (moves == 1) {
         second = 0;
         minute = 0;
         hour = 0;
         startTimer();
-    }
+        }
 
-    if (moves > 8 && moves < 12) {
-        for (i = 0; i < 3; i++) {
-            if (i > 1) {
-                /*'''''''''''
-              
-                **/
-            }
-        }
-    }
-    else if (moves > 13) {
-        for (i = 0; i < 3; i++) {
-            if (i > 0) {
-              
-        }
-    }
+  
 }
 
-/** timer clock */
+/** timer clock **/
 
 var second = 0, minute = 0; hour =0;
 var timer = document.querySelector(".timer");
@@ -84,7 +71,7 @@ function startTimer() {
            }
         }, 1000);
    }
-/**check for matching pair */
+/** check for matching pair **/
 
 
 function checkforMatch() {
@@ -96,7 +83,7 @@ function checkforMatch() {
     }
 }
 
-/**unclickable */
+/** unclickable **/
 
 function disableCards() {
     remainingCards -=2;
@@ -110,7 +97,7 @@ function disableCards() {
     }
 }
 
-/**cards dont match */
+/** cards dont match **/
 
 function unflipCards() {
 
@@ -130,7 +117,7 @@ function resetBoard() {
 
 
 }
-/**cards being shuffled into new position */
+/** cards being shuffled into new position  **/
 
 function shuffle () {
     cards.forEach(card => {
@@ -139,20 +126,17 @@ function shuffle () {
     });
 }
 
-/**well done modal */
+/** well done modal **/
 
 function congratulations() {
   
-    document.getElementById("popup1").classList.add("show");
-    
-    var starRating = document.querySelector(".stars").innerHTML;
-
+    document.getElementById("popup1").classList.add("show");  
+         
     document.getElementById("finalMove").innerHTML = moves;
-    document.getElementById("starRating").innerHTML = starRating;
     document.getElementById("totalTime").innerHTML = minute + "m " + second + "s ";
 }
 
-/**close icon */
+/** close icon **/
 
 function closeModal() {
     document.getElementById("popup1").classList.remove("show");
@@ -181,12 +165,6 @@ function resetGame() {
 
     moves = 0;
     counter.innerHTML = moves;
-
-    for (var i = 0; i < stars.length; i++){
-        stars[i].style.color = "#";
-        stars[i].style.visibility = "visible"; 
-    }
-
     second = 0;
     minute = 0;
     hour = 0;
@@ -195,6 +173,7 @@ function resetGame() {
     clearInterval(interval);
     cards.forEach(card => card.addEventListener('click' ,flipCard));
 }
+
    // Get the modal
 var modal = document.getElementById("myModal");
 
@@ -221,28 +200,3 @@ window.onclick = function(event) {
   }
 }
 
-// Get the modal
-var modal = document.getElementById("myModal");
-
-// Get the button that opens the modal
-var btn = document.getElementById("myBtn");
-
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
-
-// When the user clicks on the button, open the modal
-btn.onclick = function() {
-  modal.style.display = "block";
-}
-
-// When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-  modal.style.display = "none";
-}
-
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
-  }
-}
